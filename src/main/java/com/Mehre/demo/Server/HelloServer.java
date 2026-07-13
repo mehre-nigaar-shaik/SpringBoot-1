588312f668a48a5e47dd1b272e8a9cf3af60e7dc
mehre-nigaar-shaik <mehrenigaarshaik310@gmail.com>
2026-07-13 15:29:22 +0530

    get Hello Server

---
+++ src/main/java/com/Mehre/demo/Server/HelloServer.java
@@ -0,0 +1,12 @@
+package com.Mehre.demo.Server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloServer {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello Mehre";
+    }
+}
