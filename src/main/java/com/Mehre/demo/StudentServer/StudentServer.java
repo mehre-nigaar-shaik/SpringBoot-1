1b19f976d7a5410d644ceb49d4b3e19210960d1a
mehre-nigaar-shaik <mehrenigaarshaik310@gmail.com>
2026-07-14 12:56:42 +0530

    postwithstudent

--- src/main/java/com/Mehre/demo/StudentServer/StudentServer.java
+++ src/main/java/com/Mehre/demo/StudentServer/StudentServer.java
@@ -2,6 +2,7 @@ package com.Mehre.demo.StudentServer;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,30 +10,16 @@ public class StudentServer {
     //store the stud info , read the stud info with id, update the stud info, delete the stud info from the db
 
 @PostMapping("/create")
-public String storeStudent() {
-    return """
-            id : 1
-            name : Mehre
-            Dept : CSE
-            age : 20
-            """;
+public String storeStudent(@RequestBody Student student) {
+
+    int id = student.getId();
+    String name = student.getName();
+    int age = student.getAge();
+    String dept = student.getDept();
+
+    return  "id: " +id +
+            "name:" +name +
+            "Dept:"  +dept +
+            "age:" + age;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
